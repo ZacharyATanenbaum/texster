e8df4242a0885e41e85c36af42ee8dae35fd6445
Zach Tanenbaum <Zachtanenbaum@gmail.com>
2017-06-25 18:30:35 -0400

    Changed Java Version in Maven from 1.5 to 1.8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Zacht on 6/25/2017.
  */
@@ -6,6 +8,47 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+
+
+
+
+
+        // Start Scanning for Commands
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Accepting Commands, type \"help\" for help:");
+        String command = "";
+        String response = "";
+
+        // Scan Line
+        command = scanner.nextLine();
+
+        // Keep scanning for new commands until quit
+        while (!command.equals("quit")) {
+
+            // Check if Command
+            switch (command) {
+                case "help":
+                    response = "Command List: \n" +
+                            "help: Show list of commands \n" +
+                            "quit: Stop and close server \n";
+                    break;
+
+                default:
+                    response = command+" is not a suitable command, type \"help\" for help";
+
+            }
+
+            // Show response to command
+            System.out.println(response);
+
+            // Scan Line
+            command = scanner.nextLine();
+
+        }
+
+        // Closing Server
+        System.out.println("Closing down server.");
+        scanner.close();
 
     }
 
